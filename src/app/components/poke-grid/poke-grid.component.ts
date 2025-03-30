b8c7ac61d8181c91239c8a35ebb5e96ddec2c56e
Jamal Drake Aman <jamaldrakeaman@yahoo.com>
2025-03-30 14:53:16 +0200

    poke grid load single pokemon

--- src/app/components/poke-grid/poke-grid.component.ts
+++ src/app/components/poke-grid/poke-grid.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component,Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -9,7 +12,7 @@ import { Component,Input } from '@angular/core';
   templateUrl: './poke-grid.component.html',
   styleUrl: './poke-grid.component.scss'
 })
-export class PokeGridComponent {
+export class PokeGridComponent implements OnInit {
 
   @Input() i = 0
 
@@ -18,8 +21,41 @@ export class PokeGridComponent {
     url: ''
   }
 
+  isLoading = true;
 
-  
-  
+  pokemonData: any = {}
+
+  constructor(private http: HttpClient) { }
+
+
+  ngOnInit(): void {
+    console.log('my url', this.pokeData.url);
+    this.loadPokemon()
+  }
+
+
+  fetchDetails(): Observable<any> {
+    return this.http.get<any>(this.pokeData.url);
+  }
+
+
+  loadPokemon() {
+    this.fetchDetails().subscribe({
+      next: (response) => {
+        this.pokemonData = response;
+
+        console.log('The Response', response);
+        console.log('Gelader Pokemon:', this.pokemonData);
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error('Fehler beim Laden:', err);
+      },
+    });
+  }
+
+
+
+
 
 }
--- src/app/shared/pokemon.service.ts
+++ src/app/shared/pokemon.service.ts
@@ -16,11 +16,9 @@ export class PokemonService {
 
 
   pokemonData: any[] = [];
-  // pokeData = signal<any[]>([]);
   pokeUrl:any = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=20';
 
   constructor(private http: HttpClient) { }
-
 
   fetchDetails(): Observable<PokemonListResponse> {
     return this.http.get<PokemonListResponse>(
@@ -29,25 +27,17 @@ export class PokemonService {
   }
 
 
-  // fetchDetails(): Observable<{ results: any[] }> {
-  //   return toSignal( this.http.get<{ results: any[] }>(
-  //     'https://pokeapi.co/api/v2/pokemon?offset=0&limit=20'
-  //   ))
-  // }
-
-
   loadPokemonData() {
     this.fetchDetails().subscribe({
       next: (response) => {
         this.pokemonData = [...this.pokemonData,...response.results]; // Daten zuweisen
-        // this.pokeData.set(response.results)
+  
         console.log(this.pokeUrl);
         console.log(response);
         console.log('Geladene Pokemon:', this.pokemonData); // Korrekt ausloggen
         // console.log('Signal poke', this.pokeData);
         this.pokeUrl = response.next
         console.log(this.pokeUrl);
-        
       },
       error: (err) => {
         console.error('Fehler beim Laden:', err);
